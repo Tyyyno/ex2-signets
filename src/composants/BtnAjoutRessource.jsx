efb3ef5a4ceb94723a3dbb0411cbf12751a990da
Valentyn Kuzmenko <valiktyno@gmail.com>
2021-02-26 22:45:40 -0500

    first commit

---
+++ src/composants/BtnAjoutRessource.jsx
@@ -0,0 +1,9 @@
+import '../BtnAjoutRessource.scss';
+import Fab from '@material-ui/core/Fab';
+export default function BtnAjoutRessource() {
+  return (
+    <Fab size="medium" color="primary" className="BtnAjoutRessource">
+      +
+    </Fab>
+  );
+}
---
+++ src/composants/Dossier.jsx
@@ -0,0 +1,23 @@
+import '../Dossier.scss';
+import SortIcon from '@material-ui/icons/Sort';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import IconButton from '@material-ui/core/IconButton';
+
+export default function Dossier({id, titre, couleur, dateModif}) {
+  return (
+    <article className="Dossier">
+      <div className="couverture">
+        <SortIcon className="deplacer" >Déplacer</SortIcon>
+        <img src={`images-dossiers/${id}.png`} alt={titre}/>
+        </div>
+      <div className="info" style={{backgroundColor:couleur}}>
+        <h2>{titre}</h2>
+        <p>{dateModif}</p>
+        <IconButton className="buttonp" >
+          <MoreVertIcon/>
+        </IconButton>
+      </div>
+       
+    </article>
+  );
+}
